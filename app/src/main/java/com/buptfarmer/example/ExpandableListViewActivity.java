ebd652964666b3be187af9087a7cae658d815b03
chencheng <84365252@qq.com>
2015-02-07 23:28:27 +0800

    change:use IndexableExpandableListView

--- app/src/main/java/com/buptfarmer/example/ExpandableListViewActivity.java
+++ app/src/main/java/com/buptfarmer/example/ExpandableListViewActivity.java
@@ -2,15 +2,17 @@ package com.buptfarmer.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.ExpandableListView;
+import android.widget.SectionIndexer;
 import android.widget.TextView;
 
+import com.buptfarmer.example.widget.IndexableExpandableListView;
 import com.woozzu.android.indexablelistview.R;
 
 import java.text.SimpleDateFormat;
@@ -18,8 +20,105 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class ExpandableListViewActivity extends Activity {
-    private ExpandableListView mListView;
-    private BaseExpandableListAdapter mAdapter;
+    private static class IndexableExpandableListAdapter extends BaseExpandableListAdapter implements SectionIndexer {
+
+        private ArrayList<String> mGroupList;
+        private ArrayList<String> mChildList;
+        private LayoutInflater mInlater;
+
+        public IndexableExpandableListAdapter(Activity activity, ArrayList<String> groupList, ArrayList<String> childList) {
+            mGroupList = groupList;
+            mChildList = childList;
+            mInlater = activity.getLayoutInflater();
+            Log.d("ccc", "init size:" + mGroupList.size());
+        }
+
+        @Override
+        public Object[] getSections() {
+            Log.d("ccc", "getSections size:" + mGroupList.size());
+            String[] sections = new String[mGroupList.size()];
+            for (int index = 0; index < mGroupList.size(); index++) {
+                sections[index] = mGroupList.get(index);
+            }
+            return sections;
+        }
+
+        @Override
+        public int getPositionForSection(int sectionIndex) {
+            Log.d("ccc", "getPositionForSection sectionIndex:" + sectionIndex);
+            return sectionIndex;
+        }
+
+        @Override
+        public int getSectionForPosition(int position) {
+            return 0;
+        }
+
+
+        @Override
+        public int getGroupCount() {
+            return mGroupList.size();
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return mChildList.size();
+        }
+
+        @Override
+        public Object getGroup(int groupPosition) {
+            return mGroupList.get(groupPosition);
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return mChildList.get(childPosition);
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mInlater.inflate(R.layout.expand_list_group_layout, null);
+            }
+            TextView groupTitle = (TextView) convertView.findViewById(R.id.group_title);
+            groupTitle.setText(mGroupList.get(groupPosition));
+            return convertView;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mInlater.inflate(R.layout.expand_list_group_layout, null);
+            }
+            TextView groupTitle = (TextView) convertView.findViewById(R.id.group_title);
+            groupTitle.setText(mChildList.get(childPosition));
+            return convertView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return false;
+        }
+
+    }
+
+    private IndexableExpandableListView mListView;
+    private IndexableExpandableListAdapter mAdapter;
     private ArrayList<String> mGroutList;
     private ArrayList<String> mChildList;
     private LayoutInflater mInflater;
@@ -41,70 +140,10 @@ public class ExpandableListViewActivity extends Activity {
         for (int index = 0; index < 10; index++) {
             mChildList.add("child value:" + index);
         }
-        mListView = (ExpandableListView) findViewById(R.id.expand_list_view);
-        mAdapter = new BaseExpandableListAdapter() {
-            @Override
-            public int getGroupCount() {
-                return mGroutList.size();
-            }
-
-            @Override
-            public int getChildrenCount(int groupPosition) {
-                return mChildList.size();
-            }
-
-            @Override
-            public Object getGroup(int groupPosition) {
-                return mGroutList.get(groupPosition);
-            }
-
-            @Override
-            public Object getChild(int groupPosition, int childPosition) {
-                return mChildList.get(childPosition);
-            }
-
-            @Override
-            public long getGroupId(int groupPosition) {
-                return groupPosition;
-            }
-
-            @Override
-            public long getChildId(int groupPosition, int childPosition) {
-                return childPosition;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-
-            @Override
-            public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-                if (convertView == null) {
-                    convertView = mInflater.inflate(R.layout.expand_list_group_layout, null);
-                }
-                TextView groupTitle = (TextView) convertView.findViewById(R.id.group_title);
-                groupTitle.setText(mGroutList.get(groupPosition));
-                return convertView;
-            }
-
-            @Override
-            public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-                if (convertView == null) {
-                    convertView = mInflater.inflate(R.layout.expand_list_group_layout, null);
-                }
-                TextView groupTitle = (TextView) convertView.findViewById(R.id.group_title);
-                groupTitle.setText(mChildList.get(childPosition));
-                return convertView;
-            }
-
-            @Override
-            public boolean isChildSelectable(int groupPosition, int childPosition) {
-                return false;
-            }
-
-        };
+        mListView = (IndexableExpandableListView) findViewById(R.id.expand_list_view);
+        mAdapter = new IndexableExpandableListAdapter(this, mGroutList, mChildList);
         mListView.setAdapter(mAdapter);
+        mListView.setFastScrollEnabled(true);
     }
 
 
